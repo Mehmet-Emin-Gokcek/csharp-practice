eeabb2fd833d591114a049dfdb0b959e0ee519db
Mehmet Gokcek <gokcek50@gmail.com>
2020-10-09 00:34:33 -0500

    added attributes

--- csharp-practice/RestaurantMenu/Menu.cs
+++ csharp-practice/RestaurantMenu/Menu.cs
@@ -6,5 +6,15 @@ namespace RestaurantMenu
 {
     class Menu
     {
+
+        private DateTime lastUpdate { set; get; }
+        private List<MenuItem> items { set; get; }
+
+
+        public Menu(DateTime lastUpdate, List<MenuItem> items)
+        {
+            this.lastUpdate = lastUpdate;
+            this.items = items;
+        }
     }
 }
--- csharp-practice/RestaurantMenu/MenuItem.cs
+++ csharp-practice/RestaurantMenu/MenuItem.cs
@@ -6,5 +6,17 @@ namespace RestaurantMenu
 {
     class MenuItem
     {
+        private int price { set; get; }
+        private String description { set; get; }
+        private String category { set; get; }
+        private Boolean novelItem { set; get; }
+
+        public MenuItem(int price, string description, string category, bool novelItem)
+        {
+            this.price = price;
+            this.description = description;
+            this.category = category;
+            this.novelItem = novelItem;
+        }
     }
 }
